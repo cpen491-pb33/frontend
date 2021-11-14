436e1606880baa7575262eaacb37301606c46845
Kerem Gurel <keremgurell99@gmail.com>
2021-11-13 19:41:10 -0800

    feature: pin, drag, callout

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import MapView, { Callout, Circle, Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+	const [pin, setPin] = useState({ latitude: 37.78825, longitude: -122.4324 });
+
 	return (
 		<View style={styles.container}>
-			<Text>Test App</Text>
+			<MapView
+				style={styles.map}
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+				}}
+			>
+				<Marker
+					coordinate={pin}
+					pinColor='green'
+					draggable={true}
+					onDragStart={(e) => {
+						console.log('Drag start', e.nativeEvent.coordinates);
+					}}
+					onDragEnd={(e) => {
+						setPin({
+							latitude: e.nativeEvent.coordinate.latitude,
+							longitude: e.nativeEvent.coordinate.longitude,
+						});
+					}}
+				>
+					<Callout>
+						<Text>I am here</Text>
+					</Callout>
+				</Marker>
+				<Circle center={pin} radius={1000} />
+			</MapView>
 		</View>
 	);
 }
@@ -16,5 +46,9 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	map: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
 	},
 });
